100dfbf65495555f51038f464031f6fc4694fa11
jatin556 <43544808+jatin556@users.noreply.github.com>
2018-09-24 20:12:00 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,19 @@
+'''
+Created on Sep 17, 2018
+
+@author: jatin.pal
+'''
+# baggage=150
+# rem_baggage=0
+# no_of_baggage_picked=0
+# if(baggage>no_of_baggage_picked):
+#     no_of_baggage_picked=baggage-no_of_baggage_picked
+# while(baggage>0):
+#     rem_baggage=baggage-no_of_baggage_picked
+# print(no_of_baggage_picked)
+import random
+import math
+print(pow(2,7))
+print(math.sqrt(81))
+print(math.ceil(12))
+help(math)
